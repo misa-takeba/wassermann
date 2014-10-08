75ea2cef64437e0be324708142f0793ad005ecab
Misa Takeba <e1416mt@aiit.ac.jp>
2014-10-08 15:26:06 +0000

    Fix menus#explore

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,8 +2,23 @@ class MenusController < ApplicationController
   def index
     render
   end
+
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
+
   def explore
-    # search menu
-    redirect_to "/result"
+    @count = increment_counter
+    if @count < 3 then
+      render :action => "index"
+    else
+      redirect_to :action => "suggest"
+    end
+  end
+  
+  def suggest
   end
 end
